05e5210f92ffb7c47892ec456fea447533a50482
Joppe Boekestijn <joppeboekestijn@gmail.com>
2018-06-15 10:44:45 +0200

    adding running from terminal

--- tmp.py
+++ tmp.py
@@ -77,3 +77,53 @@
 #                   optimizer=keras.optimizers.Adadelta(),
 #                   metrics=['accuracy'])
 #     return model
+
+#
+#
+# def alex_net():
+#     model = Sequential()
+#
+#     model.add(Conv2D(filters=96, input_shape=(224, 224, 3), kernel_size=(11, 11),
+#                      strides=(4, 4), padding='valid'))
+#     model.add(Activation('relu'))
+#     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+#     model.add(BatchNormalization())
+#
+#     model.add(Conv2D(filters=256, kernel_size=(11, 11), strides=(1, 1), padding='valid'))
+#     model.add(Activation('relu'))
+#     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+#     model.add(BatchNormalization())
+#
+#     model.add(Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), padding='valid'))
+#     model.add(Activation('relu'))
+#     model.add(BatchNormalization())
+#
+#     model.add(Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), padding='valid'))
+#     model.add(Activation('relu'))
+#     model.add(BatchNormalization())
+#
+#     model.add(Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='valid'))
+#     model.add(Activation('relu'))
+#     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+#     model.add(BatchNormalization())
+#
+#     model.add(Flatten())
+#     model.add(Dense(4096, input_shape=(224 * 224 * 3,)))
+#     model.add(Activation('relu'))
+#     model.add(Dropout(0.5))
+#     model.add(BatchNormalization())
+#
+#     model.add(Dense(4096))
+#     model.add(Activation('relu'))
+#     model.add(Dropout(0.5))
+#     model.add(BatchNormalization())
+#
+#     model.add(Dense(1000))
+#     model.add(Activation('relu'))
+#     model.add(Dropout(0.5))
+#     model.add(BatchNormalization())
+#
+#     model.add(Dense(10))
+#     model.add(Activation('softmax'))
+#
+#     return model
