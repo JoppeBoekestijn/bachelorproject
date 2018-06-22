b3b884b54a3881b00d3431720cad4d602bc328fd
Joppe Boekestijn <joppeboekestijn@gmail.com>
2018-06-22 09:46:16 +0200

    evaluation

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -254,7 +254,7 @@ def evaluate(filepath):
     print('Test accuracy:', scores[1])
 
 
-def main(filepath, conv_net, data_aug):
+def main(filepath):
     """
     Main function to run convolutional neural network
     """
@@ -268,8 +268,8 @@ def main(filepath, conv_net, data_aug):
     k.tensorflow_backend.set_session(tf.Session(config=config))
 
     print(filepath)
-    print(conv_net)
-    print(data_aug)
+    # print(conv_net)
+    # print(data_aug)
 
     # Instantiate the training with chosen setting
     # training(filepath='./models/comb/googlenet_vertflip_vertshift_scratch.h5',
@@ -295,16 +295,18 @@ def main(filepath, conv_net, data_aug):
     # (data_aug = 6) == Mix-up
     # (data_aug = 7) == Cutout
 
-    training(filepath=filepath,
-             conv_net=conv_net,
-             data_aug=data_aug)
+    # training(filepath=filepath,
+    #          conv_net=conv_net,
+    #          data_aug=data_aug)
 
     # Evaluate model on test data once, without any augmentation
-    # evaluate(filepath='./models/exp2/single/googlenet_vertflip_vertshift_scratch.h5')
+    evaluate(filepath=filepath)
 
 
 if __name__ == '__main__':
     filepath = sys.argv[1]
-    conv_net = sys.argv[2]
-    data_aug = sys.argv[3]
-    main(filepath=filepath, conv_net=conv_net, data_aug=data_aug)
+    # conv_net = sys.argv[2]
+    # data_aug = sys.argv[3]
+    main(filepath=filepath)
+         # conv_net=conv_net,
+         # data_aug=data_aug)
